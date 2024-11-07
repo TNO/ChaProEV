21d5449d213654773beb46743df78d0678adb091
Omar Usmani <omar.usmani@tno.nl>
2024-11-07 04:07:52 +0100

    Added optons for fleet tables and for home type split

--- src/ChaProEV/ChaProEV.py
+++ src/ChaProEV/ChaProEV.py
@@ -106,6 +106,193 @@ except ModuleNotFoundError:
 # we are importing again
 
 
+def car_home_parking(case_name: str, general_parameters: ty.Dict) -> None:
+    home_type_parameters: ty.Dict = general_parameters['home_type']
+    input_root: str = general_parameters['files']['input_root']
+    input_folder: str = f'{input_root}/{case_name}'
+    percentages_file_name: str = home_type_parameters['percentages_file']
+    car_drivewway_percentages: pd.DataFrame = pd.read_csv(
+        f'{input_folder}/{percentages_file_name}'
+    ).set_index(home_type_parameters['index_name'])
+
+    profiles_index: ty.List[str] = home_type_parameters['profiles_index']
+    own_driveway_name: str = home_type_parameters['own_driveway_name']
+    on_street_name: str = home_type_parameters['on_street_name']
+
+    output_root: ty.Dict = general_parameters['files']['output_root']
+    output_folder: str = f'{output_root}/{case_name}'
+
+    consumption_parameters: ty.Dict = general_parameters['consumption']
+    consumption_table_name: str = consumption_parameters[
+        'consumption_table_name'
+    ]
+    use_years_in_profiles: bool = general_parameters['variants'][
+        'use_years_in_profiles'
+    ]
+
+    variant_names: ty.List[str] = list(car_drivewway_percentages.index)
+    for variant_name in variant_names:
+        own_driveway_percentage: float = float(
+            car_drivewway_percentages.loc[variant_name][
+                f'{own_driveway_name}_percentage'
+            ]
+        )
+
+        if use_years_in_profiles:
+            variant_year: str = variant_name.split('_')[-1]
+            variant_prefix: str = '_'.join(variant_name.split('_')[:-1])
+            own_driveway_profile_prefix: str = (
+                f'{variant_prefix}_{own_driveway_name}_{variant_year}'
+            )
+            on_street_profile_prefix: str = (
+                f'{variant_prefix}_{on_street_name}_{variant_year}'
+            )
+        else:
+            own_driveway_profile_prefix = f'{variant_name}_{own_driveway_name}'
+            on_street_profile_prefix = (
+                f'{output_folder}/{variant_name}_{on_street_name}'
+            )
+        own_driveway_values: pd.DataFrame = (
+            pd.read_pickle(
+                f'{output_folder}/{own_driveway_profile_prefix}_profile.pkl'
+            )
+            .reset_index()
+            .set_index(profiles_index)
+            .astype(float)
+        )
+
+        on_street_values: pd.DataFrame = (
+            pd.read_pickle(
+                f'{output_folder}/{on_street_profile_prefix}_profile.pkl'
+            )
+            .reset_index()
+            .set_index(profiles_index)
+        )
+
+        combined_values: pd.DataFrame = pd.DataFrame(
+            index=own_driveway_values.index,
+            columns=own_driveway_values.columns,
+        )
+        for quantity in combined_values.columns:
+            combined_values[quantity] = (
+                own_driveway_percentage * own_driveway_values[quantity]
+                + (1 - own_driveway_percentage) * on_street_values[quantity]
+            )
+
+        combined_values.to_pickle(
+            f'{output_folder}/{variant_name}_profile.pkl'
+        )
+
+        weekly_consumption_table: pd.DataFrame = pd.read_pickle(
+            f'{output_folder}/{own_driveway_profile_prefix}'
+            f'_{consumption_table_name}.pkl'
+        )
+        weekly_consumption_table.to_pickle(
+            f'{output_folder}/{variant_name}_{consumption_table_name}'
+        )
+
+        do_fleet_tables: float = general_parameters['profile_dataframe'][
+            'do_fleet_tables'
+        ]
+        if do_fleet_tables:
+            fleet_profiles(case_name, variant_name, general_parameters)
+
+
+def fleet_profiles(
+    case_name: str, scenario_name: str, general_parameters: ty.Dict
+) -> None:
+    home_type_parameters: ty.Dict = general_parameters['home_type']
+    consumption_parameters: ty.Dict = general_parameters['consumption']
+    consumption_table_name: str = consumption_parameters[
+        'consumption_table_name'
+    ]
+
+    distance_header: str = consumption_parameters['distance_header']
+    fleet_distance_header: str = consumption_parameters[
+        'fleet_distance_header'
+    ]
+    energy_carriers_consumption_names: ty.List[str] = consumption_parameters[
+        'energy_carriers_consumption_names'
+    ]
+    fleet_energy_carriers_consumption_names: ty.List[str] = (
+        consumption_parameters['fleet_energy_carriers_consumption_names']
+    )
+    energy_carriers: ty.List[str] = consumption_parameters['energy_carriers']
+    output_root: ty.Dict = general_parameters['files']['output_root']
+    output_folder: str = f'{output_root}/{case_name}'
+    profiles_index: ty.List[str] = home_type_parameters['profiles_index']
+    profile_headers: ty.List = general_parameters['profile_dataframe'][
+        'headers'
+    ]
+    fleet_headers: ty.List = general_parameters['profile_dataframe'][
+        'fleet_headers'
+    ]
+    input_root: str = general_parameters['files']['input_root']
+    input_folder: str = f'{input_root}/{case_name}'
+    fleet_file_name: str = general_parameters['profile_dataframe'][
+        'fleet_file_name'
+    ]
+
+    fleet_split: pd.DataFrame = pd.read_csv(
+        f'{input_folder}/{fleet_file_name}'
+    ).set_index('Variant name')
+
+    total_fleet_thousands: float = float(
+        fleet_split.loc[scenario_name]['Total Fleet (thousands)']
+    )
+    consumption_table: pd.DataFrame = pd.read_pickle(
+        f'{output_folder}/{scenario_name}_' f'{consumption_table_name}.pkl'
+    )
+    fleet_consumption_table: pd.DataFrame = pd.DataFrame(
+        index=consumption_table.index
+    )
+
+    fleet_consumption_table[fleet_distance_header] = (
+        total_fleet_thousands * consumption_table[distance_header]
+    )
+
+    for (
+        energy_carrier,
+        consumption_name,
+        fleet_consumption_name,
+    ) in zip(
+        energy_carriers,
+        energy_carriers_consumption_names,
+        fleet_energy_carriers_consumption_names,
+    ):
+        carrier_fleet_thousands: float = float(
+            total_fleet_thousands
+            * fleet_split.loc[scenario_name][f'{energy_carrier} proportion']
+        )
+        fleet_consumption_table[fleet_consumption_name] = (
+            carrier_fleet_thousands * consumption_table[consumption_name]
+        )
+        if energy_carrier == 'electricity':
+            reference_profile: pd.DataFrame = (
+                pd.read_pickle(f'{output_folder}/{scenario_name}_profile.pkl')
+                .reset_index()
+                .set_index(profiles_index)
+                .astype(float)
+            )
+            fleet_profile: pd.DataFrame = pd.DataFrame(
+                columns=fleet_headers, index=reference_profile.index
+            )
+
+            for profile_header, fleet_header in zip(
+                profile_headers, fleet_headers
+            ):
+                fleet_profile[fleet_header] = (
+                    carrier_fleet_thousands * reference_profile[profile_header]
+                )
+            fleet_profile.to_pickle(
+                f'{output_folder}/{scenario_name}_profile_fleet.pkl'
+            )
+
+    fleet_consumption_table.to_pickle(
+        f'{output_folder}/{scenario_name}_{consumption_table_name}_fleet.pkl'
+    )
+
+
 def run_scenario(
     scenario: ty.Dict, case_name: str, general_parameters: ty.Dict
 ) -> None:
@@ -213,6 +400,12 @@ def run_scenario(
     output_folder: str = f'{output_root}/{case_name}'
     profile_dataframe.to_pickle(f'{output_folder}/{scenario_name}_profile.pkl')
 
+    do_fleet_tables: float = general_parameters['profile_dataframe'][
+        'do_fleet_tables'
+    ]
+    if do_fleet_tables:
+        fleet_profiles(case_name, scenario_name, general_parameters)
+
 
 def load_scenarios(case_name: str) -> ty.List[ty.Dict]:
     scenario_folder_files: ty.List[str] = os.listdir(f'scenarios/{case_name}')
@@ -264,6 +457,12 @@ def run_ChaProEV(case_name: str) -> None:
             run_scenario,
             zip(scenarios, repeat(case_name), repeat(general_parameters)),
         )
+
+    do_car_home_type_split: bool = general_parameters['home_type'][
+        'do_car_home_type_split'
+    ]
+    if do_car_home_type_split:
+        car_home_parking(case_name, general_parameters)
 
     write_start: datetime.datetime = datetime.datetime.now()
     writing.extra_end_outputs(case_name, general_parameters)
